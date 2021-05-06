82513c70065023bc5cf40d38fdeee12dbb38fbbe
yizaochen <yizaochen@gmail.com>
2021-05-06 16:08:24 +0800

    Update

--- Diffusity/src/Diffusity.jl
+++ Diffusity/src/Diffusity.jl
@@ -5,4 +5,12 @@ function sigmoid(x_center::Real, x::Real, scale_factor::Real, translation_factor
     return (one(z) / (one(z) + exp(-z))) * scale_factor + translation_factor
 end
 
+function linear(xleft::Real, xright::Real, yleft::Real, yright::Real, x::Real, c::Real)
+    """
+    model: y = mx + c
+    """
+    m = (yright - yleft) / (xright - xleft)
+    return m * x + c
+end
+
 end # module
