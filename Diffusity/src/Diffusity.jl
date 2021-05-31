27ec25875494e32225dc1ed87e28ac0769ad0013
yizaochen <yizaochen@gmail.com>
2021-05-31 20:59:09 +0800

    Update

--- Diffusity/src/Diffusity.jl
+++ Diffusity/src/Diffusity.jl
@@ -176,19 +176,16 @@ function get_l_derivative_taylor_v1(D::Real, lambda0_array::Array{Float64,1}, de
     return py_derivative / py 
 end
 
-function update_D(Np::Int64, Δt::Real, Nv::Int64, alpha_t0::Array{Float64,2}, D_old::Real, lambda0_array::Array{Float64,1}, Qx::Array{Float64,2}, xref::Array{Float64,2}, e_norm::Float64, interpo_xs::Array{Float64,1}, w0::Array{Float64,2},y_record::Array{Float64,2}, k_delta::Real)
-    upper_term = 0.
-    bottom_term = 0.
-
-    g_array = zeros(Np)
-    aj_mat = zeros((Np, Nv))
-    bj_mat = zeros((Np, Nv))
-    cj_mat = zeros((Np, Nv))
+function update_D(Np::Int64, Nphoton::Int64, Δt::Real, Nv::Int64, alpha_t0::Array{Float64,2}, D_old::Real, lambda0_array::Array{Float64,1}, Qx::Array{Float64,2}, xref::Array{Float64,2}, e_norm::Float64, interpo_xs::Array{Float64,1}, w0::Array{Float64,2},y_record::Array{Float64,2}, k_delta::Real)
+    g_array = zeros(Nphoton)
+    aj_mat = zeros((Nphoton, Nv))
+    bj_mat = zeros((Nphoton, Nv))
+    cj_mat = zeros((Nphoton, Nv))
 
     alpha = zeros(Nv, 1)
     alpha[:,1] = alpha_t0
     
-    for τ = 1:Np
+    for τ = 1:Nphoton
         y = y_record[τ+1]
         photon_mat = get_photon_matrix_gaussian(y, xref, e_norm, interpo_xs, Np, w0, k_delta)
         psi_photon_psi = Qx' * photon_mat * Qx
@@ -198,13 +195,82 @@ function update_D(Np::Int64, Δt::Real, Nv::Int64, alpha_t0::Array{Float64,2}, D
         bj_mat[τ,:] = [get_bj(j, Nv, alpha, psi_photon_psi) for j=1:Nv]
         cj_mat[τ,:] = [get_cj(j, Nv, lambda0_array, alpha, psi_photon_psi) for j=1:Nv]
 
-        expLQDT = exp.(-(D_old * lambda0_array) .* save_freq)
+        expLQDT = exp.(-(D_old * lambda0_array) .* Δt)
         alpha_e_delta_t = expLQDT .* alpha
         alpha_next =  psi_photon_psi * alpha_e_delta_t
         alpha[:,1] = alpha_next / norm(alpha_next)
     end
-    D = upper_term / (Δt * bottom_term)
-    return D
+
+    upper_term = sum([(1/g_array[τ]) * dot(bj_mat[τ,:], cj_mat[τ,:]) for τ = 1:Nphoton])
+    bottom_term = sum([(1/g_array[τ]) * (dot(aj_mat[τ,:], bj_mat[τ,:]) - dot(cj_mat[τ,:],cj_mat[τ,:])) for τ = 1:Nphoton])
+    return upper_term / (Δt * bottom_term)
+end
+
+### Analytical Get log likelihood
+function get_loglikelihood_analytical(Np::Int64, Nphoton::Int64, Δt::Real, Nv::Int64, alpha_t0::Array{Float64,2}, D::Real, lambda0_array::Array{Float64,1}, Qx::Array{Float64,2}, xref::Array{Float64,2}, e_norm::Float64, interpo_xs::Array{Float64,1}, w0::Array{Float64,2},y_record::Array{Float64,2}, k_delta::Real)
+    g_array = zeros(Nphoton)
+
+    alpha = zeros(Nv, 1)
+    alpha[:,1] = alpha_t0
+
+    for τ = 1:Nphoton
+        y = y_record[τ+1]
+        photon_mat = get_photon_matrix_gaussian(y, xref, e_norm, interpo_xs, Np, w0, k_delta)
+        psi_photon_psi = Qx' * photon_mat * Qx
+
+        g_array[τ] = sum(get_fjD(D, lambda0_array, Δt, j, Nv, alpha, psi_photon_psi)^2 for j=1:Nv)
+
+        expLQDT = exp.(-(D * lambda0_array) .* Δt)
+        alpha_e_delta_t = expLQDT .* alpha
+        alpha_next =  psi_photon_psi * alpha_e_delta_t
+        alpha[:,1] = alpha_next / norm(alpha_next)
+    end
+    return sum(log.(g_array))
+end
+
+function get_dldD_analytical(Np::Int64, Nphoton::Int64, Δt::Real, Nv::Int64, alpha_t0::Array{Float64,2}, D::Real, lambda0_array::Array{Float64,1}, Qx::Array{Float64,2}, xref::Array{Float64,2}, e_norm::Float64, interpo_xs::Array{Float64,1}, w0::Array{Float64,2},y_record::Array{Float64,2}, k_delta::Real)
+    g_array = zeros(Nphoton)
+    dgdD_array = zeros(Nphoton)
+
+    alpha = zeros(Nv, 1)
+    alpha[:,1] = alpha_t0
+
+    for τ = 1:Nphoton
+        y = y_record[τ+1]
+        photon_mat = get_photon_matrix_gaussian(y, xref, e_norm, interpo_xs, Np, w0, k_delta)
+        psi_photon_psi = Qx' * photon_mat * Qx
+
+        g_array[τ] = sum(get_fjD(D, lambda0_array, Δt, j, Nv, alpha, psi_photon_psi)^2 for j=1:Nv)
+        dgdD_array[τ] = get_py_derivative(D, lambda0_array, Δt, Nv, alpha, psi_photon_psi)
+
+        expLQDT = exp.(-(D * lambda0_array) .* Δt)
+        alpha_e_delta_t = expLQDT .* alpha
+        alpha_next =  psi_photon_psi * alpha_e_delta_t
+        alpha[:,1] = alpha_next / norm(alpha_next)
+    end
+    return dot(1/g_array, dgdD_array)
+end
+
+
+### Numerical Get log likelihood
+function get_loglikelihood_numerical(Np::Int64, Nphoton::Int64, Δt::Real, Nv::Int64, alpha_t0::Array{Float64,2}, D::Real, lambda0_array::Array{Float64,1}, Qx::Array{Float64,2}, xref::Array{Float64,2}, e_norm::Float64, interpo_xs::Array{Float64,1}, w0::Array{Float64,2},y_record::Array{Float64,2}, k_delta::Real)
+    alpha_norm_array = zeros(Nphoton)
+
+    alpha = zeros(Nv, 1)
+    alpha[:,1] = alpha_t0
+
+    for τ = 1:Nphoton
+        y = y_record[τ+1]
+        photon_mat = get_photon_matrix_gaussian(y, xref, e_norm, interpo_xs, Np, w0, k_delta)
+        psi_photon_psi = Qx' * photon_mat * Qx
+
+        expLQDT = exp.(-(D * lambda0_array) .* Δt)
+        alpha_e_delta_t = expLQDT .* alpha
+        alpha_next =  psi_photon_psi * alpha_e_delta_t
+        alpha_norm_array[τ] = norm(alpha_next)
+        alpha[:,1] = alpha_next / alpha_norm_array[τ]
+    end
+    return 2 * sum(log.(alpha_norm_array))
 end
 
 
